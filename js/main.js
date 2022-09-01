c481716a3f637cbc2bdf8c5ace9c5936e1906565
user <user@users-MacBook-Pro.local>
2022-09-01 14:47:20 +0400

    finally

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $('.banner-carousel').owlCarousel({
             items:1
         },
         600:{
-            items:3
+            items:1
         },
         1000:{
             items:1
@@ -26,10 +26,49 @@ $('.ambulance-carousel').owlCarousel({
             items:1
         },
         600:{
-            items:3
+            items:1
         },
         1000:{
             items:1
+        }
+    }
+})
+
+
+$('.doctor-owl').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:true,
+    navText: ["<img src='img/leftox.svg'>","<img src='img/leftox.svg'>"],
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:4
+            
+        }
+    }
+})
+
+
+$('.feedback-owl ').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    navText: ["<img src='img/leftox.svg'>","<img src='img/leftox.svg'>"],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
         }
     }
 })
